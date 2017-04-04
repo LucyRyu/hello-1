7c22ce7d501f1238994e2537766a8494f4121a3f
soongon <soongon@gmailcom>
2017-04-04 11:06:54 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Created by danawacomputer on 2017-04-03.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        int howManyDays = 90;
+        int hoursPerDay = 8;
+
+        int totalHours = howManyDays * hoursPerDay;
+
+        System.out.println("그럼 모두 몇시간? " + totalHours);
+    }
+}
---
+++ src/MyAccountBook.java
@@ -0,0 +1,14 @@
+/**
+ * Created by danawacomputer on 2017-04-03.
+ */
+public class MyAccountBook {
+    public static void main(String[] args) {
+        int busFee   = 1600;
+        int forLunch = 7500;
+        int latteHot = 2900;
+
+        int totalSpentForToday = busFee + forLunch + latteHot;
+
+        System.out.println("오늘 쓴 경비는 " + totalSpentForToday + "원 입니다.");
+    }
+}
